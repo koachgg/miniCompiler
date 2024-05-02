61de85e7ef78ff55704c6fd08e108ee8197fe74c
Belo Abhigyan <132650323+koachgg@users.noreply.github.com>
2024-05-02 20:22:07 +0530

    Add files via upload

---
+++ Intermediate Code Generator/quadruple/input.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+		int a=10;
+        int b=20;
+        while( a > b ){
+                a = a+1;
+        }
+        if( b < = c ){
+                a = 10;
+        }
+        else{
+                a = 20;
+        }
+        a = 100;
+	for(i=0;i<10;i = i+1){
+	        a = a+1;
+	}
+	(x < b) ? x = 10 : x=11;
+	
+}
